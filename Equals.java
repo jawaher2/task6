747bd2dbd1c60f7c4a06e2da1d383217d66c667f
jawaher2 <79042182+jawaher2@users.noreply.github.com>
2021-03-27 21:48:52 +0300

    Add files via upload

---
+++ Equals.java
@@ -0,0 +1,23 @@
+/**
+ * Created by shaker on 03/27/2021.
+ */
+public class Equals {
+    if(isEmpty()) {
+    throws new Empty();
+    }
+    int count = 1;
+    Node hrunner = head;
+    Node trunner = tail;
+while(hrunner!=trunner && hrunner.getNext()!=trunner) {
+        count++; count++;
+        hrunner = hrunner.getNext();
+        trunner = trunner.getNext();
+    }
+if(hrunner!=trunner) {
+
+        count--;
+    }
+
+
+
+}
